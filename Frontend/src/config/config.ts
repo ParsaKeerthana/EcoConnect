b2291ffce293bea5fcf453652ea29883be10a630
Keerthana <keerthanaparsa23@gmail.com>
2025-04-26 19:34:03 -0700

    Updates config.ts

--- Frontend/src/config/config.ts
+++ Frontend/src/config/config.ts
@@ -1,10 +1,10 @@
 const Config = {
-    USER_SERVICE_URL: 'http://localhost:8088/api/users',
-    POST_SERVICE_URL: 'http://localhost:8088/api/posts',
-    NOTIFICATION_SERVICE_URL: 'http://localhost:8088/api/notifications/preferences',
-    MARKETPLACE_SERVICE_URL: 'http://localhost:8088/api/marketplace',
-    FEED_SERVICE_URL: 'http://localhost:8088/api/feed',
-    EVENT_SERVICE_URL: 'http://localhost:8088/api/events',
+    USER_SERVICE_URL: '/api/users',
+    POST_SERVICE_URL: '/api/posts',
+    NOTIFICATION_SERVICE_URL: '/api/notifications/preferences',
+    MARKETPLACE_SERVICE_URL: '/api/marketplace',
+    FEED_SERVICE_URL: '/api/feed',
+    EVENT_SERVICE_URL: '/api/events',
     CHATBOT_SERVICE_URL: 'http://127.0.0.1:5000/api/chat',
 };
 
